b8ecfd17d4cbc0316f67154a4d33f01375409226
Pierce <piercenhughes1@yahoo.com>
2025-01-18 15:51:22 -0600

    Created Navbars/retail product holder box

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,62 @@
-import { motion } from "motion/react"
+import * as motion from "motion/react-client"
+import { Square_Peg } from "next/font/google";
+
+{/* Define box size used for retail items */}
+const box = {
+  width: 100,
+  height: 100,
+  backgroundColor: "white",
+  borderRadius: 5,
+}
 
 export default function Home() {
   return ( 
     // Navbar
     <div>
-      <li className="font-Geist text-white">Products</li>
-      <li className="text-white">About</li>
-      <li className="text-white">Contact</li>
-      <h1 className="text-white">THC-ASAP</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-      Shop
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a className="flex items-center space-x-3 rtl:space-x-reverse">
+        {/* Logo Image */}
+        <img src="" className="h-8" alt="" />
+        {/* Logo Text */}
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">THC-ASAP</span>
+      </a>
+      <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span className="sr-only">Open main menu</span>
+      {/* Mobile Menu */}
+      <svg className="w-10 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
       </button>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-      Cart
-      </button>
-      {/* End of Navbar */}
-      {/* Floating top bar */}
+      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.8 }}
+            style={box}
+        />
+      {/* End of first Navbar */}
+      {/* Floating Navbar top start */}
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div>
       <div>
@@ -32,7 +73,5 @@ export default function Home() {
     </div>
     </div>
     </div>
-    // End of floating top bar
-    // Idea for items to expand on click to reveal more details https://codesandbox.io/p/sandbox/framer-motion-app-store-demo-su6mx?file=%2Fsrc%2FApp.js%3A31%2C2&from-embed
   );
 }
